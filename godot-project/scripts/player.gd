93439b902176c40f3633493401aa51a3c784f904
Therealtin <112870726+Therealtin@users.noreply.github.com>
2026-06-10 10:40:48 +1000

    feat | created codeblock and player scenes

---
+++ godot-project/scripts/player.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+var velocity: Vector2 = Vector2(0, 0);
+var speed: float = 60
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#position = Vector2(0, 0);
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta;
+	
+	var direction = get_direction()
+	velocity = direction * speed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if velocity.length() > 0:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
+
+func input(key):
+	pass
+	
+func get_direction() -> Vector2:
+	var direction = Vector2(0, 0)
+	if Input.is_action_pressed("move left"):
+		direction += Vector2(-1, 0)
+	if Input.is_action_pressed("move right"):
+		direction += Vector2(1, 0)
+	if Input.is_action_pressed("move up"):
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("move down"):
+		direction += Vector2(0, 1)
+	
+	direction = direction.normalized();
+	#print(direction)
+	print(position)
+	return direction;
+		
