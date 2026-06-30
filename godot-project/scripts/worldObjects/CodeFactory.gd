a7b9fa9783646e9b9b0bc5aa5f5e90fb9ceda2c4
Chocsheep <gabi.novak21@gmail.com>
2026-06-30 21:29:08 +1000

    making factory produce codeblocks

--- godot-project/scripts/worldObjects/CodeFactory.gd
+++ godot-project/scripts/worldObjects/CodeFactory.gd
@@ -6,6 +6,8 @@ extends Interactable
 @onready var pickupComponent: PlayerPickupComponent = $PlayerPickupComponent
 @onready var collisionShape: CollisionShape2D = $CollisionShape2D
 
+@export var blockScene: PackedScene
+
 func onEnterInteractRange():
 	pass
 
@@ -23,11 +25,20 @@ func onDrop():
 	collisionShape.call_deferred("disabled", false)
 
 func onPlayerInteract(player: Player):
-	if pickupComponent.isPlayerAttached():
-		pickupComponent.getDropped(player)
-	else:
-		pickupComponent.getPickedUp(player)
-		player.speed = 200
+	spawnBlock(player)
+	#if pickupComponent.isPlayerAttached():
+		#pickupComponent.getDropped(player)
+	#else:
+		#pickupComponent.getPickedUp(player)
+		#player.speed = 200
+
+func spawnBlock(player: Player):
+	var block = blockScene.instantiate()
+	World.getWorld().add_child(block)
+	block.global_position = global_position
+
+	var pickupComponent = block.get_node("PlayerPickupComponent")
+	pickupComponent.getPickedUp(player)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
